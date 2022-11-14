e60a78355c3a3737958f0361572ae0b2101f49d1
D Sai Sashreek <saisashreekd@gmail.com>
2022-11-14 14:16:37 +0530

    Layout Setup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
+import { ContextProvider } from "./contexts/ContextProvider";
+
+// ReactDOM.render(
+//   <ContextProvider>
+//     <App />
+//   </ContextProvider>,
+//   document.getElementById("root")
+// );
+
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-root.render(<App />);
+root.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
